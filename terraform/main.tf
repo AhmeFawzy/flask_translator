51898f5ce38568447f5b83b902aca1f163fe48c5
Mohamed Khaled <m.khaled191298@gmail.com>
2026-03-10 15:19:20 +0200

    ec2 removed

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,17 +10,6 @@ module "network" {
 }
 
 
-module "ec2" {
-  source = "./modules/ec2"
-
-  ami             = "ami-0b6c6ebed2801a5cb"
-  instance_type   = "t2.micro"
-  subnet_id       = module.network.public_subnet_ids[0]
-  vpc_id          = module.network.vpc_id
-
-  key_name        = "my-keypair"
-  public_key_path = "~/.ssh/id_rsa.pub"
-}
 
 
 module "eks" {
